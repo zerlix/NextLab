c441725f9a2971a64444162b36b60a240256cee2
zerlix <t.hemm@t-online.de>
2024-12-21 14:03:40 +0100

    init next with material

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Button from '@/components/Button'
+ 
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Button />
+    </div>
+  )
+}
---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button, Typography } from '@mui/material';
+
+const DarkRoundedButton = () => {
+  return (
+    <Button variant="contained" color="primary">
+      Klick mich
+    </Button>
+  )
+}
+
+export default DarkRoundedButton
