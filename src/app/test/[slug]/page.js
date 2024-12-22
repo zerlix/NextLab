2db4f24e76362291aa0d391fb2e29b143ae33b64
zerlix <t.hemm@t-online.de>
2024-12-22 11:13:30 +0100

    noch ein fix...

--- src/app/test/[slug]/page.js
+++ src/app/test/[slug]/page.js
@@ -1,5 +1,10 @@
-export default function Post({ params }) {
-  const { slug } = params;
+"use client";
+
+import { usePathname } from 'next/navigation';
+
+export default function Post() {
+  const pathname = usePathname();
+  const slug = pathname.split("/").pop();  // Holt den dynamischen Teil der URL
 
   return <div>Blog Post: {slug}</div>;
 }
