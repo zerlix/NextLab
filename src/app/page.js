3f4df6aeffa25aeea972daddd2e20e31f916ac49
Zerlix <t.hemm@t-online.de>
2024-12-25 14:40:34 +0100

    new file:   src/app/content.mdx

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import TextTest from '@/components/Typography';
+import Content from './content.mdx'; // Importiert die MDX-Datei
+import { ThemeProviderComponent } from '@/components/themes/ThemeProvider'; // Importiert den ThemeProvider
+
+
 export default function HomePage() {
   return (
     <>
-    <TextTest />
+        <Content /> {/* Rendert den Inhalt der MDX-Datei */}
     </>
   );
-    
-    
 }
 
