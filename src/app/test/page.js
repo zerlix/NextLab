b473a8a020d35ba29e506e1cf4f52e00f8a82955
Zerlix <t.hemm@t-online.de>
2024-12-25 10:44:27 +0100

    db test

--- src/app/test/page.js
+++ src/app/test/page.js
@@ -1,5 +1,29 @@
-// app/test/page.js
+import { PrismaClient } from '@prisma/client';
 
-export default function TestPage() {
-  return <div>Welcome to the Test Page</div>;
+// Singleton PrismaClient to prevent multiple instances
+const globalForPrisma = global;
+globalForPrisma.prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+
+export default async function Page() {
+  try {
+    const posts = await prisma.user.findMany();
+    
+    return (
+      <div>
+        <h1>User</h1>
+        <ul>
+          {posts.map((post) => (
+            <li key={post.id}>
+              {post.title}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  } catch (error) {
+    console.error('Error fetching posts:', error);
+    throw new Error('Failed to fetch posts');
+  }
 }
